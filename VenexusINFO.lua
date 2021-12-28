3a9af0f8d9e76e8ce954747ddf7499c9a9f9d754
a <74766072+1xcbn@users.noreply.github.com>
2021-12-28 22:48:10 +0700

    Update and rename README.md to VenexusINFO.lua

---
+++ VenexusINFO.lua
@@ -0,0 +1,39 @@
+--[[
+
+██╗░░░██╗███████╗███╗░░██╗███████╗██╗░░██╗██╗░░░██╗░██████╗  ██╗███╗░░██╗███████╗░█████╗░
+██║░░░██║██╔════╝████╗░██║██╔════╝╚██╗██╔╝██║░░░██║██╔════╝  ██║████╗░██║██╔════╝██╔══██╗
+╚██╗░██╔╝█████╗░░██╔██╗██║█████╗░░░╚███╔╝░██║░░░██║╚█████╗░  ██║██╔██╗██║█████╗░░██║░░██║
+░╚████╔╝░██╔══╝░░██║╚████║██╔══╝░░░██╔██╗░██║░░░██║░╚═══██╗  ██║██║╚████║██╔══╝░░██║░░██║
+░░╚██╔╝░░███████╗██║░╚███║███████╗██╔╝╚██╗╚██████╔╝██████╔╝  ██║██║░╚███║██║░░░░░╚█████╔╝
+░░░╚═╝░░░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝░╚═════╝░╚═════╝░  ╚═╝╚═╝░░╚══╝╚═╝░░░░░░╚════╝░
+
+]]
+
+Welcome to VeNexus, to use the scripts, paste the scripts in your executor!
+
+Currently supported exploits:
+* Synapse X
+* KRNL
+* Oxygen-U
+* Sentinel
+* Scriptwares
+
+Supported games:
+* **Break-In**
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+* Coming-Soon
+
+**Discords : 'currently down'**
+
+**Founder : !ven#6793**
