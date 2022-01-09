f9932982b7238129965583f8f0ff2da7e1ae8c33
a <74766072+1xcbn@users.noreply.github.com>
2022-01-09 16:45:23 +0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -19,7 +19,7 @@ local themes = {
 	Accent = Color3.fromRGB(72, 145, 217), 
 	LightContrast = Color3.fromRGB(18, 18, 18), 
 	DarkContrast = Color3.fromRGB(14, 14, 14),  
-	TextColor = Color3.fromRGB(255, 255, 255)
+	TextColor = Color3.fromRGB(85, 170, 255)
 }
 
 do
