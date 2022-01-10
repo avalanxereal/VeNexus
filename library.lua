0b550c37077c26111c9fc37e667e90a35893a6c9
a <74766072+1xcbn@users.noreply.github.com>
2022-01-10 19:21:55 +0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -682,7 +682,7 @@ do
 				Text = title,
 				TextColor3 = themes.TextColor,
 				TextSize = 12,
-				TextXAlignment = Enum.TextXAlignment.Left
+				TextXAlignment = Enum.TextXAlignment.Left,
 				TextTransparency = 0.10000000149012
 			})
 		})
